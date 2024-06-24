ff29788b4de8c9757b18c0a164b3122eca142dc6
Helena Miura <helenamiura@gmail.com>
2024-06-24 12:07:24 +0200

    added lists and methods to add and remove elements

---
+++ DictionnaryCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿Dictionary<char, string> letters = new Dictionary<char, string>();
+letters.Add('a', "première lettre de l'alphabet");
+letters.Add('b', "deuxème lettre de l'alphabet");
+letters.Add('c', "troisième lettre de l'alphabet");
+
+Console.WriteLine("Dictionary:");
+foreach (var letter in letters) {
+    Console.WriteLine($"Key: {letter.Key}; Value: {letter.Value}");
+}
+
+letters.Remove('c');
+
+Console.WriteLine("\nDictionary après remove lettre C:");
+foreach (var letter in letters) {
+    Console.WriteLine($"Key: {letter.Key}; Value: {letter.Value}");
+}
